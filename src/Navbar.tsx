377fb0a0e5f94158f949e5bc456dee3881cbe0f7
Mireia <66952824+miiree12lb@users.noreply.github.com>
2024-10-24 16:27:26 +0200

    navbar update

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,15 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+// @ts-ignore
+import about_me from "./assets/images/nav icons/about_me.png";
+// @ts-ignore
+import education from "./assets/images/nav icons/education.png";
+// @ts-ignore
+import experience from "./assets/images/nav icons/experience.png";
+// @ts-ignore
+import achievements from "./assets/images/nav icons/achievements.png";
+// @ts-ignore
+import projects from "./assets/images/nav icons/projects.png";
+// @ts-ignore
+import contact from "./assets/images/nav icons/contact.png";
 
 export default function Navbar() {
     return (
         <>
-            <Link to="/">About Me</Link>
-            <Link to="education">Education</Link>
-            <Link to="experience">Experience</Link>
-            <Link to="achievements">Achievements</Link>
-            <Link to="projects">Projects</Link>
-            <Link to="contact">Contact Me</Link>
+            <NavLink to="/" className="nav-item" activeClassName="active">
+                <span className="nav-text">About Me</span>
+                <span className="nav-icon"><img src={about_me} alt="About Me" /></span>
+            </NavLink>
+
+            <NavLink to="/education" className="nav-item" activeClassName="active">
+                <span className="nav-text">Education</span>
+                <span className="nav-icon"><img src={education} alt="Education" /></span>
+            </NavLink>
+
+            <NavLink to="/experience" className="nav-item" activeClassName="active">
+                <span className="nav-text">Experience</span>
+                <span className="nav-icon"><img src={experience} alt="Experience" /></span>
+            </NavLink>
+
+            <NavLink to="/achievements" className="nav-item" activeClassName="active">
+                <span className="nav-text">Achievements</span>
+                <span className="nav-icon"><img src={achievements} alt="Achievements" /></span>
+            </NavLink>
+            
+            <NavLink to="/projects" className="nav-item" activeClassName="active">
+                <span className="nav-text">Projects</span>
+                <span className="nav-icon"><img src={projects} alt="Projects" /></span>
+            </NavLink>
+            
+            <NavLink to="/contact" className="nav-item" activeClassName="active">
+                <span className="nav-text">Contact Me</span>
+                <span className="nav-icon"><img src={contact} alt="Contact Me" /></span>
+            </NavLink>
         </>
     );
 }
