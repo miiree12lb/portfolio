8e20b79959964f406a4669ef7b156c8428bcc791
Mireia <66952824+miiree12lb@users.noreply.github.com>
2024-10-24 19:19:24 +0200

    phone navbar with selected element golden background

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -12,38 +12,87 @@ import achievements from "./assets/images/nav icons/achievements.png";
 import projects from "./assets/images/nav icons/projects.png";
 // @ts-ignore
 import contact from "./assets/images/nav icons/contact.png";
+// Selected tab images (or use different images if desired)
+// @ts-ignore
+import selected_about_me from "./assets/images/nav icons/about_meBlack.png";
+// @ts-ignore
+import selected_education from "./assets/images/nav icons/educationBlack.png";
+// @ts-ignore
+import selected_experience from "./assets/images/nav icons/experienceBlack.png";
+// @ts-ignore
+import selected_achievements from "./assets/images/nav icons/achievementsBlack.png";
+// @ts-ignore
+import selected_projects from "./assets/images/nav icons/projectsBlack.png";
+// @ts-ignore
+import selected_contact from "./assets/images/nav icons/contactBlack.png";
 
 export default function Navbar() {
     return (
         <>
             <NavLink to="/" className="nav-item" activeClassName="active">
-                <span className="nav-text">About Me</span>
-                <span className="nav-icon"><img src={about_me} alt="About Me" /></span>
+                {({ isActive }) => (
+                    <>
+                        <span className="nav-text">About Me</span>
+                        <span className="nav-icon">
+                            <img src={isActive ? selected_about_me : about_me} alt="About Me" />
+                        </span>
+                    </>
+                )}
             </NavLink>
 
             <NavLink to="/education" className="nav-item" activeClassName="active">
-                <span className="nav-text">Education</span>
-                <span className="nav-icon"><img src={education} alt="Education" /></span>
+                {({ isActive }) => (
+                    <>
+                        <span className="nav-text">Education</span>
+                        <span className="nav-icon">
+                            <img src={isActive ? selected_education : education} alt="Education" />
+                        </span>
+                    </>
+                )}
             </NavLink>
 
             <NavLink to="/experience" className="nav-item" activeClassName="active">
-                <span className="nav-text">Experience</span>
-                <span className="nav-icon"><img src={experience} alt="Experience" /></span>
+                {({ isActive }) => (
+                    <>
+                        <span className="nav-text">Experience</span>
+                        <span className="nav-icon">
+                            <img src={isActive ? selected_experience : experience} alt="Experience" />
+                        </span>
+                    </>
+                )}
             </NavLink>
 
             <NavLink to="/achievements" className="nav-item" activeClassName="active">
-                <span className="nav-text">Achievements</span>
-                <span className="nav-icon"><img src={achievements} alt="Achievements" /></span>
+                {({ isActive }) => (
+                    <>
+                        <span className="nav-text">Achievements</span>
+                        <span className="nav-icon">
+                            <img src={isActive ? selected_achievements : achievements} alt="Achievements" />
+                        </span>
+                    </>
+                )}
             </NavLink>
-            
+
             <NavLink to="/projects" className="nav-item" activeClassName="active">
-                <span className="nav-text">Projects</span>
-                <span className="nav-icon"><img src={projects} alt="Projects" /></span>
+                {({ isActive }) => (
+                    <>
+                        <span className="nav-text">Projects</span>
+                        <span className="nav-icon">
+                            <img src={isActive ? selected_projects : projects} alt="Projects" />
+                        </span>
+                    </>
+                )}
             </NavLink>
-            
+
             <NavLink to="/contact" className="nav-item" activeClassName="active">
-                <span className="nav-text">Contact Me</span>
-                <span className="nav-icon"><img src={contact} alt="Contact Me" /></span>
+                {({ isActive }) => (
+                    <>
+                        <span className="nav-text">Contact Me</span>
+                        <span className="nav-icon">
+                            <img src={isActive ? selected_contact : contact} alt="Contact Me" />
+                        </span>
+                    </>
+                )}
             </NavLink>
         </>
     );
