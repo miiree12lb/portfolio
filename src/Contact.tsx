9dfbd23c66c83c87784e3a013f7edd1f12d9d85b
Mireia <66952824+miiree12lb@users.noreply.github.com>
2024-12-19 15:36:16 +0100

    add portugal + improve form and map

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -10,16 +10,16 @@ export default function Contact(){
                 
                     <form target="_blank" rel="noopener noreferrer" action="https://formspree.io/f/mldeqdjb" method="POST">
                         <label htmlFor="firstName">First Name</label>
-                        <input type="text"id="firstName"name="First Name:" required />
+                        <input type="text"id="firstName"name="First Name" required />
 
                         <label htmlFor="lastName">Last Name</label>
-                        <input type="text"id="lastName"name="lastName" required />
+                        <input type="text"id="lastName"name="Last Name" required />
 
                         <label htmlFor="email">Email</label>
-                        <input type="email"id="email"name="email" required />
+                        <input type="email"id="email"name="Email" required />
 
                         <label htmlFor="message">Message</label>
-                        <textarea id="message" name="message" rows="10" cols="35" required></textarea>
+                        <textarea id="message" name="Message" rows="10" cols="35" required></textarea>
                         
                         
                         <button type="submit" id="submit">Submit</button>
--- src/MapChart.tsx
+++ src/MapChart.tsx
@@ -7,7 +7,7 @@ const geoUrl =
 const visitedCountries = [
   "United States of America", "Spain", "France", "Italy", "Germany", "United Kingdom",
   "Belgium", "Ireland", "Netherlands", "Switzerland", "Norway", "Sweden", "Turkey", "Greece",
-  "Croatia", "Slovenia", "Luxembourg", "Monaco", "Andorra"
+  "Croatia", "Slovenia", "Luxembourg", "Monaco", "Andorra", "Portugal"
 ];
 
 export default function MapChart() {
