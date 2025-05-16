f4890cae4eb05b89590c23b3d12ac6cc88e48952
Mireia <66952824+miiree12lb@users.noreply.github.com>
2025-05-16 19:20:24 +0200

    update progress again

--- src/Education.tsx
+++ src/Education.tsx
@@ -497,7 +497,7 @@ export default function Education(){
         </ul>
     </div>
 
-    const credits = 132;
+    const credits = 135;
 
     return <>
         <Popup isOpen={isSpanishBaccalaureatePopupOpen} onRequestClose={closeSpanishBaccalaureatePopup} title="Spanish Baccalaureate" content={spanishBaccalaureatePopupContent} />
